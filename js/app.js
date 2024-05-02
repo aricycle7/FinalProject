e2e13ab11a9f4df6653d4aaef9259d044123e3bb
why he ourple <146386988+vpMcGraw@users.noreply.github.com>
2024-05-02 10:30:22 -0400

    navbar :3

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 let nav_button = document.getElementsByClassName('nav_icon');
 let nav = document.getElementsByClassName('nav')[0];
 
+let nav_exit_index = document.getElementsByClassName('nav_icon_exit')
+
     nav_button[0].addEventListener("click", function() {
         this.classList.toggle("active");
 
@@ -13,5 +15,14 @@ let nav = document.getElementsByClassName('nav')[0];
         else { 
             console.log('woosh')
             nav.style.display = "block";
+        }
+    })
+
+    nav_exit_index[0].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        if (nav.style.display.match("block")) {
+            console.log('pee')
+            nav.style.display = "none";
         }
     })
