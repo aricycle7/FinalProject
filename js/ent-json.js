76cbca0ef698f2b6fba2390262e00c37e4e3db24
why he ourple <146386988+vpMcGraw@users.noreply.github.com>
2024-05-20 10:36:51 -0400

    dfsfdsdfs

--- js/ent-json.js
+++ js/ent-json.js
@@ -17,7 +17,7 @@ xhr4.onload = function () {
       let newCard ="";
       newCard += "<p>" + responseObject.CCT[i].name; 
       newCard += "</p>";
-/*      newCard += "<a href='" + responseObject.CCT[i].imgLink + "'>" + "<img src='" + responseObject.CCT[i].img + "'alt='nananakcsnklna' height='40%' width='40%'></a>";  */
+      newCard += "<a href='" + responseObject.CCT[i].imgLink + "'>" + "<img src='" + responseObject.CCT[i].img + "'alt='nananakcsnklna' height='300px' width='400px'></a>"; 
       document.getElementById("ecard_sect4").innerHTML += newCard;
     }
 
@@ -25,28 +25,31 @@ xhr4.onload = function () {
       let newCard ="";
       newCard += "<p>" + responseObject.LegoLand[i].name;
       newCard += "</p>";
-      document.getElementById("ecard_sect2").innerHTML += newCard;
+      newCard += "<a href='" + responseObject.LegoLand[i].imgLink + "'>" + "<img src='" + responseObject.LegoLand[i].img + "'alt='nananakcsnklna' height='300px' width='500px'></a>"; 
+      document.getElementById("ecard_sect3").innerHTML += newCard;
     }
 
     for (let i=0; i<responseObject.Otherworld.length; i++){
       let newCard ="";
       newCard += "<p>" + responseObject.Otherworld[i].name;
       newCard += "</p>";
-      document.getElementById("ecard_sect3").innerHTML += newCard;
+      newCard += "<a href='" + responseObject.Otherworld[i].imgLink + "'>" + "<img src='" + responseObject.Otherworld[i].img + "'alt='nananakcsnklna' height='500px' width='550px'></a>";
+      document.getElementById("ecard_sect5").innerHTML += newCard;
     }
 
     for (let i=0; i<responseObject.COSI.length; i++){
       let newCard ="";
       newCard += "<p>" + responseObject.COSI[i].name;
       newCard += "</p>";
-      document.getElementById("ecard_sect5").innerHTML += newCard;
+      newCard += "<a href='" + responseObject.COSI[i].imgLink + "'>" + "<img src='" + responseObject.COSI[i].img + "'alt='nananakcsnklna' height='500px' width='250px'></a>"; 
+      document.getElementById("ecard_sect1").innerHTML += newCard;
     }
-
+/* 
     for (let i=0; i<responseObject.PolarisMall.length; i++){
       let newCard ="";
       newCard += "<p>" + responseObject.PolarisMall[i].name;
       newCard += "</p>";
-      document.getElementById("ecard_sect1").innerHTML += newCard;
+      document.getElementById("ecard_sect2").innerHTML += newCard;
     }
 
     for (let i=0; i<responseObject.ZoombeziBay.length; i++){
@@ -55,11 +58,12 @@ xhr4.onload = function () {
       newCard += "</p>";
       document.getElementById("ecard_sect6").innerHTML += newCard;
     }
-
+ */
     for (let i=0; i<responseObject.NorthMarket.length; i++){
       let newCard ="";
       newCard += "<p>" + responseObject.NorthMarket[i].name;
       newCard += "</p>";
+      newCard += "<a href='" + responseObject.NorthMarket[i].imgLink + "'>" + "<img src='" + responseObject.NorthMarket[i].img + "'alt='nananakcsnklna' height='250px' width='700px'></a>"; 
       document.getElementById("ecard_sect7").innerHTML += newCard;
     }
 
